587286db38874473aefc5e9e587179c7055b6bfd
dumitr58 <robert@DESKTOP-SAGMNUN.localdomain>
2020-03-21 20:00:19 -0400

    Updates

---
+++ precache-manifest.13f429d367c3eca1d4cb989e45eb9287.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c63663fbf9882d21e29a452281a35e",
+    "url": "/Resume/index.html"
+  },
+  {
+    "revision": "c0bffb5872bc97b805b0",
+    "url": "/Resume/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "45ce9e2a9f69bcb2765e",
+    "url": "/Resume/static/css/main.54082345.chunk.css"
+  },
+  {
+    "revision": "c0bffb5872bc97b805b0",
+    "url": "/Resume/static/js/2.609ffed9.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Resume/static/js/2.609ffed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ce9e2a9f69bcb2765e",
+    "url": "/Resume/static/js/main.49e04266.chunk.js"
+  },
+  {
+    "revision": "a8bd4d37f56736925879",
+    "url": "/Resume/static/js/runtime-main.04142021.js"
+  },
+  {
+    "revision": "3654a70e85e6a2bcc8464909440d6540",
+    "url": "/Resume/static/media/Capture.3654a70e.PNG"
+  },
+  {
+    "revision": "cfcaa2419f16a2bdfca47e2ab7db87ee",
+    "url": "/Resume/static/media/RDLogo.cfcaa241.svg"
+  }
+]);
